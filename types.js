bef9586c5722c3be99d9edc01e354be55af9713b
jeff <10823453+jeffmc@users.noreply.github.com>
2022-02-16 07:01:46 -0800

    Added Vectors and LaneQuery functionality

--- types.js
+++ types.js
@@ -6,26 +6,56 @@ const DIV_STRIPE_SIZE = 30;
 
 const TWEEN_LANE_FACTOR = 0.05;
 
+const DEBUG_PDG = 2;
+
 class Traffic {
   constructor() {
     this.cars = [];
-    this.queryStarts = [];
-    this.width = HIGHWAY_WIDTH;
-    this.height = LANE_HEIGHT *LANES + DIV_HEIGHT * (LANES+1);
+    this.queryChainHeads = [];
+
     let sport = Car.sports();
     this.addCar(sport, 0);
     this.addCar(Car.semi(), 1);
-    this.addCar(Car.suv(), 2);
-    sport.targetY = this.getCarY(sport,3);
+    this.addCar(Car.semi(), 2);
+    this.addCar(Car.suv(), 3);
+    sport.attemptMergeDown();
   }
+  get width() { return HIGHWAY_WIDTH; }
+  get height() { return LANE_HEIGHT *LANES + DIV_HEIGHT * (LANES+1); }
+
   insertQuery(query) {
-    // TODO: add
+    let head = this.queryChainHeads[query.laneIdx];
+    if (head == null || head == undefined) { // Empty lane
+      this.queryChainHeads[query.laneIdx] = query;
+      query.next = query;
+      query.last = query;
+    } else if (head.next == head && head.last == head) { // Solo lane
+      head.next = query;
+      head.last = query;
+      
+      query.next = head;
+      query.last = head;
+
+    } else { // Multi-car lane
+      // Find leftmost
+      let leftmost = head;
+      while (true) {
+        if (leftmost.last.x > leftmost.x) break; else {
+          leftmost = leftmost.last;
+        }
+      }
+      // Find correct gap
+
+    }
+  }
+  removeQuery(query) {
+    // TODO: Implement this.
   }
   addCar(car,lane) {
     this.cars.push(car);
     car.traffic = this;
     car.y = this.getCarY(car, lane);
-    car.realQuery.laneIdx = lane;
+    car.realQuery = new LaneQuery(new Vector(), car.lane, car, true);
     this.insertQuery(car.realQuery);
   }
   tick() {
@@ -37,7 +67,15 @@ class Traffic {
     this.drawRoads(gfx);
     for (const car of this.cars) {
       car.render(gfx);
-    } 
+    }
+    for (const head of this.queryChainHeads) {
+      if (head == null) continue;
+      let next = head;
+      do {
+        next.render(gfx);
+        next = next.next;
+      } while (next != head && next != null);
+    }
   }
   drawRoads(gfx) {
     gfx.fillStyle = "#222"; // road color.
@@ -83,35 +121,43 @@ class Car {
     return new Car(0,0,50,20,"#22a", 0.02, 2);
   }
   constructor(x,y,w,h,c,a,t) {
-    this.transform = [x,y,w,h];
+    this.transform = new Vector(x,y,w,h);
     this.color = c;
     this.acceleration = a;
     this.topSpeed = t;
     this.speed = 0;
     this.traffic = null;
-    this.targetY = null;
-    this.movingLanes = false;
-    this.realQuery = new LaneQuery(this.transform,-1,this,true);
-  }
-  get x() { return this.transform[0]; }
-  set x(v) { this.transform[0] = v; }
-
-  get y() { return this.transform[1]; }
-  set y(v) { this.transform[1] = v; }
-
-  get centerY() { 
-    return this.transform[1] + this.transform[3] / 2;
+    // this.targetY = null;
+    // this.movingLanes = false;
+    this.realQuery = null;
+    this.testQuery = null;
   }
 
-  get width() { return this.transform[2]; }
-  set width(v) { this.transform[2] = v; }
+  get x() { return this.transform.x; }
+  set x(v) { this.transform.x = v; }
+  get y() { return this.transform.y; }
+  set y(v) { this.transform.y = v; }
 
-  get height() { return this.transform[3]; }
-  set height(v) { this.transform[3] = v; }
+  get centerX() { return this.transform.centerX; }
+  get centerY() { return this.transform.centerY; }
 
-  changeLane(lane) {
-    this.targetY = this.traffic.getCarY(this,lane);
+  get width() { return this.transform.width; }
+  set width(v) { this.transform.width = v; }
+  get height() { return this.transform.height; }
+  set height(v) { this.transform.height = v; }
+
+  get lane() { return this.traffic.getLaneAtY(this.centerY); }
+
+  cleanCurrentTestQuery() {
+    if (this.testQuery == null) return;
+    this.traffic.removeQuery(this.testQuery); 
   }
+  attemptMergeDown() {
+    this.cleanCurrentTestQuery();
+    this.testQuery = new LaneQuery(new Vector(0,LANE_HEIGHT+DIV_HEIGHT),this.lane+1,this,false);
+    this.traffic.insertQuery(this.testQuery);
+  }
+
   tick() {
     if (this.speed < this.topSpeed) {
       this.speed += this.acceleration;
@@ -120,18 +166,19 @@ class Car {
     }
     this.x += this.speed;
     if (this.x > this.traffic.width) this.x = -this.width; // TODO: Add throughput measuring here.
-    if (this.targetY) {
-      this.movingLanes = true;
-      let delta = ( this.targetY - this.y ) * TWEEN_LANE_FACTOR;
-      if (Math.abs(delta) < TWEEN_LANE_FACTOR) {
-        this.y = this.targetY;
-        this.targetY = null;
-      } else {
-        this.y += delta;
-      }
-    } else {
-      this.movingLanes = false;
-    }
+
+    // if (this.targetY) {
+    //   this.movingLanes = true;
+    //   let delta = ( this.targetY - this.y ) * TWEEN_LANE_FACTOR;
+    //   if (Math.abs(delta) < TWEEN_LANE_FACTOR) {
+    //     this.y = this.targetY;
+    //     this.targetY = null;
+    //   } else {
+    //     this.y += delta;
+    //   }
+    // } else {
+    //   this.movingLanes = false;
+    // }
   }
   render(gfx) {
     gfx.fillStyle = this.color;
@@ -140,11 +187,13 @@ class Car {
 }
 
 class LaneQuery {
-  constructor(transform, laneIdx, car, real) {
-    this.transform = transform;
+  constructor(offset, laneIdx, car, real) {
+    this.offset = offset; // Vector
     this.laneIdx = laneIdx;
     this.owner = car; 
     this.real = real;
+
+    // Chain/Linked list members 
     this.next = null;
     this.last = null;
   }
@@ -153,6 +202,65 @@ class LaneQuery {
   }
   render(gfx) {
     gfx.strokeStyle = "#aaa";
-    gfx.strokeRect(this.x,this.y,this.width,this.height);
+    gfx.strokeRect(this.x, this.y, this.width, this.height);
+    if (!this.real) gfxDrawLine(gfx, this.centerX, this.centerY, this.owner.centerX, this.owner.centerY);
+    if (this.next != this) {
+      gfx.strokeStyle = "#2aa";
+      gfxDrawLine(gfx, this.centerX, this.centerY+DEBUG_PDG, this.next.centerX, this.next.centerY+DEBUG_PDG);
+      gfx.strokeStyle = "#a2a";
+      gfxDrawLine(gfx, this.centerX, this.centerY-DEBUG_PDG, this.last.centerX, this.last.centerY-DEBUG_PDG);
+    }
   }
+
+  get x() { return this.owner.x + this.offset.x; }
+  get y() { return this.owner.y + this.offset.y; }
+  get width() { return this.owner.width; }
+  get height() { return this.owner.height; }
+  
+  get centerX() { return this.owner.centerX + this.offset.x; }
+  get centerY() { return this.owner.centerY + this.offset.y; }
+
+}
+
+class Vector {
+  constructor(x = 0, y = 0, w = 0, h = 0) {
+    this.val = [x,y,w,h]
+  }
+  pos(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+  size(w,h) {
+    this.w = w;
+    this.h = h;
+  }
+  set(x,y,w,h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  get x() { return this.val[0]; }
+  set x(v) { this.val[0] = v; }
+  get y() { return this.val[1]; }
+  set y(v) { this.val[1] = v; }
+  get w() { return this.val[2]; }
+  set w(v) { this.val[2] = v; }
+  get h() { return this.val[3]; }
+  set h(v) { this.val[3] = v; }
+
+  get width() { return this.w; }
+  set width(v) { this.w = v; }
+  get height() { return this.h; }
+  set height(v) { this.h = v; }
+
+  get centerX() { return this.x + this.w / 2};
+  get centerY() { return this.y + this.h / 2};
+}
+
+function gfxDrawLine(gfx,x1,y1,x2,y2) {
+  gfx.beginPath();
+  gfx.moveTo(x1,y1);
+  gfx.lineTo(x2,y2);
+  gfx.stroke();
 }
